691d831d4b71c5a3dc82e4cea2a723b82421eec7
AelqarMrc <160648229+AelqarMrc@users.noreply.github.com>
2024-05-01 10:19:21 +0200

    new commit

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,8 +1,32 @@
-import MyClass from "./assets/js/MyClass.js"
+// import MyClass from "../assets/js/MyClass.js"
 
 
 
-alert("ok")
+console.log("ok");
+
+fetch('http://localhost:5678/api/works')
+  .then(response => response.json())
+  .then(data => {
+      data.forEach((item,i) => {
+          document.querySelector(".gallery").innerHTML += `<figure>
+          <img src="${item.imageUrl}" alt="Abajour Tahina">
+          <figcaption>${item.title}</figcaption>
+        </figure>`        
+      });}
+        )
+  .catch(error => console.error('Erreur lors de la récupération des données:', error));
 
 
+  fetch('http://localhost:5678/api/categories')
+  
+    .then(response => response.json())
+  
+    .then(data => {
+          console.log(data);
+          data.forEach((item, index) => {
+          // CODE ??
+    });}
+        )
+    .catch(error => console.log(error));
 
+    
